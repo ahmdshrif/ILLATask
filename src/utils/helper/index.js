75b795ba2116faf2940e49347dbf99b1274d8261
ahmed ashraf <a.s1171111@gmail.com>
2020-12-04 13:53:06 +0200

    upgrade async storage to supported packege .

--- src/utils/helper/index.js
+++ src/utils/helper/index.js
@@ -1,5 +1,4 @@
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-import {AsyncStorage} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const FAVORITES_KEY = '@ILLA:Favorites';
 
