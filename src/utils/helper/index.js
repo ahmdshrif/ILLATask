e2ad0b76c26260b137930a5df5996ee769335797
ahmed ashraf <a.s1171111@gmail.com>
2020-12-04 09:04:32 +0200

    helper function /(favorites)

---
+++ src/utils/helper/index.js
@@ -0,0 +1,39 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const FAVORITES_KEY = '@ILLA:Favorites';
+
+/**
+ * @returns all favorite products
+ */
+export const getFavorites = async () => {
+  try {
+    const jsonValue = await AsyncStorage.getItem(FAVORITES_KEY);
+    return jsonValue != null ? JSON.parse(jsonValue) : {};
+  } catch (e) {}
+};
+
+
+/**
+ * @param item item to set in storage {object}
+ * @returns  favorite products
+ */
+export const setFavorite = async (item) => {
+  try {
+    let favorites = await getFavorites();
+    favorites[item.id] = item;
+    return await AsyncStorage.setItem(FAVORITES_KEY, JSON.stringify(favorites));
+  } catch (error) {}
+};
+
+
+/**
+ * @param item item to remove from favorite storage {object}
+ * @returns  favorite products
+ */
+export const removeFavorite = async (item) => {
+  try {
+    let favorites = await getFavorites();
+    delete favorites[item.id];
+    return await AsyncStorage.setItem(FAVORITES_KEY, JSON.stringify(favorites));
+  } catch (error) {}
+};
