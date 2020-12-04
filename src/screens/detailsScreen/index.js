9169afc081420503ea50a29600dc316a5086782a
ahmed ashraf <a.s1171111@gmail.com>
2020-12-04 10:27:11 +0200

    add Favorit functionality

--- src/screens/detailsScreen/index.js
+++ src/screens/detailsScreen/index.js
@@ -1,10 +1,49 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Text, View, Image, StyleSheet, ScrollView, Button} from 'react-native';
 import PropTypes from 'prop-types';
+import {getFavorites, removeFavorite, setFavorite} from '../../utils/helper';
 
 const Details = ({route}) => {
-  const {name, description, price, discount_amount, image} = route.params.Data;
+  const {
+    name,
+    description,
+    price,
+    discount_amount,
+    image,
+    id,
+  } = route.params.Data;
   const [isFavorite, setIsFavorite] = useState(false);
+  useEffect(() => {
+    _getFavorites();
+  }, []);
+  const _getFavorites = async () => {
+    try {
+      const Favorites = await getFavorites();
+      console.log(Favorites);
+      setIsFavorite(!!Favorites[id]);
+    } catch (error) {
+      alert('some thing went wrong');
+    }
+  };
+  const _setToFavorite = async () => {
+    try {
+      setIsFavorite(true); //optimistic update
+      await setFavorite(route.params.Data);
+    } catch (error) {
+      setIsFavorite(false); //undo update
+      alert('some thing went wrong');
+    }
+  };
+
+  const _removeFromFavorite = async () => {
+    try {
+      setIsFavorite(false); //optimistic update
+      await removeFavorite(route.params.Data);
+    } catch (error) {
+      setIsFavorite(true); //undo update
+      alert('some thing went wrong');
+    }
+  };
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <View style={{flex: 1, alignItems: 'center'}}>
@@ -14,15 +53,9 @@ const Details = ({route}) => {
         <Text style={styles.text}>{`Price : ${price}`}</Text>
         <Text style={styles.text}>{`Discount :  ${discount_amount}`}</Text>
         {isFavorite ? (
-          <Button
-            title={' Remove Favorite  '}
-            onPress={() => setIsFavorite(false)}
-          />
+          <Button title={' Remove Favorite  '} onPress={_removeFromFavorite} />
         ) : (
-          <Button
-            title={'  Add to Favorite  '}
-            onPress={() => setIsFavorite(true)}
-          />
+          <Button title={'  Add to Favorite  '} onPress={_setToFavorite} />
         )}
       </View>
     </ScrollView>
